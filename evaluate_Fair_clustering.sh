4b25a30d2809893e02408a52e49ffd68dee94aba
imtiazziko <ziko.iut@gmail.com>
2019-06-11 16:36:36 -0400

    Upload code

--- evaluate_Fair_clustering.sh
+++ evaluate_Fair_clustering.sh
@@ -1,9 +1,7 @@
-# python test_fair_clustering.py -d Synthetic-unequal \
-#                              --cluster_option kmeans \
-#                              --plot_option_clusters_vs_lambda \
-#                              --plot_option_fairness_vs_clusterE \
-#                              --plot_option_convergence 
-python test_fair_clustering.py -d  Bank \
-                             --cluster_option kmeans 
+python test_fair_clustering.py -d Synthetic-unequal \
+                             --cluster_option kmeans \
+                             --plot_option_clusters_vs_lambda \
+                             --plot_option_fairness_vs_clusterE \
+                             --plot_option_convergence 
 
 
